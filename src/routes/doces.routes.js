abfe1506132eb8f68dbca317017e1cacc8db99b8
Luiz Aureliano <luizaureliano.tds1.senai@gmail.com>
2024-09-05 11:44:29 -0300

    create delete method

--- src/routes/doces.routes.js
+++ src/routes/doces.routes.js
@@ -1,7 +1,7 @@
 import { response, Router } from "express";
 const docesRoutes = Router();
 
-const guloseimas = [
+let guloseimas = [
   {
     id: 1,
     nome: "Trufa",
@@ -63,15 +63,35 @@ docesRoutes.put("/:id", (req, res) => {
     return res.status(404).send({ message: "guloseima não encontrada!!!!!!!" });
   }
 
-  const { nome, preco} = req.body;
+  const { nome, preco } = req.body;
 
-  guloseima.nome = nome
-  guloseima.preco = preco
+  guloseima.nome = nome;
+  guloseima.preco = preco;
 
   return res.status(200).send({
-    message:"Guloseima atualizada !!!!!!",guloseima
-  })
+    message: "Guloseima atualizada !!!!!!",
+    guloseima,
+  });
 });
 
+// rota para deletar uma guloseima
+docesRoutes.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  
+  const guloseima = guloseimas.find((doce) => doce.id === Number(id));
+  console.log(guloseima);
+  
+  if (!guloseima) {
+    return res.status(404).send({ message: "guloseima não encontrada!!!!!!!" });
+  }
+  
+  guloseimas = guloseimas.filter((doce) => doce.id !== Number(id));
+
+  return res.status(200).send({
+    message: "guloseima deletada",
+    guloseima,
+  })
+});
 
 export default docesRoutes;
